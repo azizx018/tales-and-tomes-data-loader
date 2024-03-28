0093f50be5f1fc5de3ecb707afd2e1391a0d500a
azizx018 <chiara.aziz@gmail.com>
2024-03-28 13:45:53 -0400

    setup Cassandra connection

--- src/main/java/io/javabrains/talesandtomesdataloader/TalesAndTomesDataLoaderApplication.java
+++ src/main/java/io/javabrains/talesandtomesdataloader/TalesAndTomesDataLoaderApplication.java
@@ -1,13 +1,28 @@
 package io.javabrains.talesandtomesdataloader;
 
+import java.nio.file.Path;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import connection.DataStaxAstraProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class TalesAndTomesDataLoaderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TalesAndTomesDataLoaderApplication.class, args);
 	}
+
+    //giving spring data cass the ability to connect to instance
+    @Bean
+	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
+        Path bundle = astraProperties.getSecureConnectBundle().toPath();
+        return builder -> builder.withCloudSecureConnectBundle(bundle);
+ }
 
 }

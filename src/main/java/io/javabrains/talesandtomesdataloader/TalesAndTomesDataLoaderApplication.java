c20d94a16bf54557f65bd75da65d4f11d13b87ae
azizx018 <chiara.aziz@gmail.com>
2024-03-28 11:48:53 -0400

    first commit

---
+++ src/main/java/io/javabrains/talesandtomesdataloader/TalesAndTomesDataLoaderApplication.java
@@ -0,0 +1,13 @@
+package io.javabrains.talesandtomesdataloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalesAndTomesDataLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalesAndTomesDataLoaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/javabrains/talesandtomesdataloader/TalesAndTomesDataLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javabrains.talesandtomesdataloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalesAndTomesDataLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
